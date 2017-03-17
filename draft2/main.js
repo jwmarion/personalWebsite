8f2733e0d017aa3ba790014cb4c5b6072260612c
James M <jwmarion@gmail.com>
2017-03-16 21:07:06 -0400

    fixed layuot

--- draft2/main.js
+++ draft2/main.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
+$('#fullpage').fullpage({
+  // easingcss3: 'ease',
 
+});
 
-var $target = $('.wrapper');
-
-inView('.section').on('enter', function(el){
-  var color = $(el).attr('data-background-color');
-  $target.css('background-color', color );
+// var $target = $('#fullpage');
+//
+// inView('.section').on('enter', function(el){
+//   var color = $(el).attr('data-background-color');
+//   $target.css('background-color', color );
+// });
 });
 
 
-});
 
 // window.onload = $(".firstPage").css('background-color', '#D1DCBC' );
 // // });
