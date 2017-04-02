265b77224444ac3425b6387fd7e513ce09dc2866
James M <jwmarion@gmail.com>
2017-04-01 20:49:42 -0400

    added more animation

--- main.js
+++ main.js
@@ -3,17 +3,6 @@ $(document).ready(function () {
   $('#fullpage').fullpage({
     anchors:['page1','page2','page3','page4'],
     controlArrows:false,
-    onLeave: function(index, nextIndex, direction) {
-      if(index == 2 && direction =='up'){
-        console.log('test');
-  		}
-      else {
-        TweenLite.to($('.bubbleWave'), 1, {
-          top: -50,
-          right:0
-        });
-      }
-    }
   });
   $('.projects').on('click','img:nth-of-type(1)', function(){
     $.fn.fullpage.moveTo('page2', 1);
