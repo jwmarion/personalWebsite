385ce27b99b399bc3c4f11574c02640213ba4ac6
James M <jwmarion@gmail.com>
2017-04-20 16:17:14 -0400

    added huddleup

--- main.js
+++ main.js
@@ -6,16 +6,13 @@ $(document).ready(function () {
   });
   $('.projects').on('click','img:nth-of-type(1)', function(){
     $.fn.fullpage.moveTo('page2', 1);
-    console.log('yes');
   });
   $('.projects').on('click','img:nth-of-type(2)', function(){
     $.fn.fullpage.moveTo('page2', 2);
-    console.log('yes');
   });
-  // $('.projects').on('click','img:nth-of-type(3)', function(){
-  //   $.fn.fullpage.moveTo('page2', 3);
-  //   console.log('yes');
-  // });
+  $('.projects').on('click','img:nth-of-type(3)', function(){
+    $.fn.fullpage.moveTo('page2', 3);
+  });
   $('.pRightBlock').on('click','img:nth-of-type(2)', function(){
     $.fn.fullpage.moveTo('page2', 0);
     console.log('yes');
